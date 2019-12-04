81132a04b3b8307c5cc188cc247ef59fa8b68a5d
humble-goat <gatziaris.pantelis@gmail.com>
2019-12-04 05:43:04 +0200

    Add project files.

---
+++ CompanyIntelGatherer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompanyIntelGatherer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the company\'s afm:");
+        }
+    }
+}
